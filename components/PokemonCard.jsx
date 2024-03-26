0b28cbaa9695a9b52acd13c1bb24649e577cb5e8
VINCENT VAN VOLSEM <vincent.vanvolsem@gmail.com>
2024-03-26 15:49:42 +0100

    rendre le composant dynamique

--- components/PokemonCard.jsx
+++ components/PokemonCard.jsx
@@ -1,12 +1,32 @@
 const PokemonCard = () => {
-    return (
+    const pokemon = pokemonList[0]
+    
+        return (
         <figure>
-<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+            {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc}  alt={pokemon.name} />
+            ):(
+                <p>???</p>
+            )}
+            
         
-            <figcaption>Bulbasaur</figcaption>
+            <figcaption> {pokemon.name} </figcaption>
 
         </figure>
+
+
     );
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default PokemonCard;
